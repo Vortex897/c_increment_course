2e85b9b4644e405cc91ab22c465e510e09327949
Hendrill <etaleliton@gmail.com>
2025-09-02 10:21:08 -0300

    Create for_ex2.cpp

---
+++ for_ex2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void cleanScreen() {
+    for (int i = 0; i < 100; i++){
+        cout << "\n";
+    }
+}
+
+int main() {
+    cleanScreen();
+    for (int i = 1; i <= 10; i++) {
+        cout << "5 * " << i << " = " << i * 5 << endl;
+    }
+    
+    return 0;
+}
